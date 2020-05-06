62e589efaf7470965fd883d06101e0d92a3b2805
fasnimo <fasnimo@gmail.com>
2020-05-05 19:43:14 -0500

    Looking to add faker

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :plants
   resources :zodiacs
   resources :relations
-
+  # get '/test', to: 'application#test'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
