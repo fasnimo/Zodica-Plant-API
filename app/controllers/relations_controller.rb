cbf4873bd961874e76e3248c20416e30dc48e57b
fasnimo <fasnimo@gmail.com>
2020-05-11 10:30:38 -0500

    repository has frontend and backend folder that needs to be pushed.

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -1,26 +1,31 @@
 class RelationsController < ApplicationController
     def index
-        binding.pry
-        if params[:zodiac_id]
-            zodiac = Zodiac.find(params[:zodiac_id])
-            zodiac_relations = zodiac.relations.all
-            render json: RelationSerializer.new(zodiac_relations)
-    
-        end
+        #  binding.pry
+        # if params[:zodiac_id]
+        #     zodiac = Zodiac.find(params[:zodiac_id])
+        #     zodiac_relations = zodiac.relations.all
+        #     render json: RelationSerializer.new(zodiac_relations)
+        # end
+        relations = Relation.all
+        render json: RelationSerializer.new(relations)
     end
 
     def show
-        if params[:zodiac_id] 
-            zodiac = Zodiac.find(params[:zodiac_id])
-            relation = zodiac.relations.find(params[:id])
-            render json: RelationSerializer.new(relation) 
-        end
+        relation = Relation.find(params[:id])
+        render json: RelationSerializer.new(relation)
+        # if params[:zodiac_id] 
+        #     zodiac = Zodiac.find(params[:zodiac_id])
+        #     relation = zodiac.relations.find(params[:id])
+        #     render json: RelationSerializer.new(relation) 
+        # end
     end 
 
     def createZodiacRelation
-        if params[:zodiac_id]
+        if params[:zodiac_id] && params[:plant_id]
+            plant = Plant.find(params[:plant_id])
             zodiac = Zodiac.find(params[:zodiac_id])
             relation = zodiac.relations.build()
+            relation = plant.relations.build()
             if relation.save
                 binding.pry
                 render json: RelationSerializer.new(relation)
@@ -28,15 +33,15 @@ class RelationsController < ApplicationController
         end
     end
 
-    def newrelation
-        # zodiac = Zodiac.find_or_create_by(name: params[:_json][0][:name].downcase, month: params[:_json][0][:month].downcase)
-        # plant = Plant.find_or_create_by(name: params[:_json][1][:name].downcase)
-        # relation = Relation.create(zodiac_id: zodiac.id, plant_id: plant.id)
-        render json: {
-            # plant: PlantSerializer.new(plant),
-            # zodiac: ZodiacSerializer.new(zodiac),
-            relation: RelationSerializer.new(relation)
-        } 
-    end 
+    # def newrelation
+    #     # zodiac = Zodiac.find_or_create_by(name: params[:_json][0][:name].downcase, month: params[:_json][0][:month].downcase)
+    #     # plant = Plant.find_or_create_by(name: params[:_json][1][:name].downcase)
+    #     # relation = Relation.create(zodiac_id: zodiac.id, plant_id: plant.id)
+    #     render json: {
+    #         # plant: PlantSerializer.new(plant),
+    #         # zodiac: ZodiacSerializer.new(zodiac),
+    #         relation: RelationSerializer.new(relation)
+    #     } 
+    # end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  resources :zodiacs do 
-    resources :relations
-  end
+  # resources :zodiacs do 
+  #   resources :relations
+  # end
 
-  resources :plants do 
-    resources :relations
-  end
-  # resources :zodiacs
-  # resources :plants
-  # resources :relations
+  # resources :plants do 
+  #   resources :relations
+  # end
+  resources :zodiacs
+  resources :plants
+  resources :relations
 
   # get 'newrelation', to: 'relations#newrelation'
   # post 'newrelation', to: 'relations#newrelation'
+  get "/zodiac_relations", to: "relations#createZodiacRelation"
   post "/zodiac_relations", to: "relations#createZodiacRelation"
   # get '/test', to: 'application#test'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
